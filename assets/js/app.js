a603d76202c0412d21ba7a31fe33d3333bc8d737
Emre Eroğlu <emre1eroglu7@gmail.com>
2024-01-29 20:22:05 +0300

    project gitgub add

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+function hamb(){
+   
+    document.querySelector("#header-list").style.display="block";
+    document.querySelector("#header-ico").style.display="block";
+    document.querySelector("#hamb-ico1").style.display="none";
+document.querySelector("#hamb-ico2").style.display="block";
+    
+   
+}
+
+function hamb1(){
+   
+    document.querySelector("#header-list").style.display="none";
+    document.querySelector("#header-ico").style.display="none";
+    document.querySelector("#hamb-ico1").style.display="block";
+document.querySelector("#hamb-ico2").style.display="none";
+    
+
+   
+}
+
+
